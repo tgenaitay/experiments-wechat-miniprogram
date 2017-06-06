6e149001552b5c3e90c5d38346b57bb4b17c6541
thibault genaitay <tgenaitay@gmail.com>
2017-06-06 13:18:17 +0800

    experimenting with wx.vibrateLong

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,25 +1,31 @@
 // index.js
 var app = getApp()
 Page({
-  // build rough data for the page
-  data: {
-    brief: 'Send a brief in 1 minute',
-    userInfo: {}
-  },
   // load data and log success
   onLoad: function () {
     console.log('index page loaded')
-    var that = this
-    app.getUserInfo(function(userInfo){
-      that.setData({
-        userInfo:userInfo
-      })
+    wx.getSystemInfo({
+      success: function (res) {
+        console.log(res.model)
+        console.log(res.pixelRatio)
+        console.log(res.windowWidth)
+        console.log(res.windowHeight)
+        console.log(res.language)
+        console.log(res.version)
+        console.log(res.platform)
+      }
     })
   },
-  onShareAppMessage: function () {
-    return {
-      title: 'Le Wagon coding school',
-      path: 'pages/index/index'
+  vibratePhone: function (e) {
+    if (wx.vibrateLong) {
+      wx.vibrateLong()
+    } else {
+      // can't use
+      wx.showModal({
+        title: 'Problem',
+        content: 'The Vibrate API is not available on this phone'
+      })
     }
+    console.log("vibrate!!")
   }
 })
