c0415a2bb0deb5fcb4f55d4e7cf8fb13eedf938f
thibault genaitay <tgenaitay@gmail.com>
2017-06-07 00:02:58 +0800

    added animations through wx.createAnimation

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,17 +3,47 @@ var app = getApp()
 Page({
   // build rough data for the page
   data: {
-    CTA: 'Click me please',
-    userInfo: {}
+    btn: {CTA: 'Make me swing!', hide: 'Make it disappear..'},
+    poof: '',
+    animationData: {}
   },
   // load data and log success
   onLoad: function () {
     console.log('index page loaded')
-    var that = this
-    app.getUserInfo(function(userInfo){
-      that.setData({
-        userInfo:userInfo
-      })
+  },
+  swingMe: function() {
+    // setup the animation config
+    var animation = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-in-out',
+    })
+    this.animation = animation
+    console.log("make the swing");
+    
+    // define the animation steps
+    animation.rotate3d(0, 0, 1, 15).step()
+    animation.rotate3d(0, 0, 1, -10).step()
+    animation.rotate3d(0, 0, 1, 5).step()
+    animation.rotate3d(0, 0, 1, -5).step()
+    animation.rotate3d(0, 0, 1, 0).step()
+    
+    // store it in local data to send to the view
+    this.setData({
+      animationData: {cta: animation.export()}
+    })
+  },
+  hideIt: function () {
+    var animation = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'ease-in-out',
+    })
+    this.animation = animation
+    console.log("make the button disappear");
+    animation.opacity(0).step()
+    this.setData({
+      animationData: { cta: animation.export() },
+      poof: 'poof!'
     })
   }
 })
+
