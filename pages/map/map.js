363e1bcc4f3c350c07feb664f8456e8ea0926404
thibault genaitay <tgenaitay@gmail.com>
2017-06-06 19:26:49 +0800

    added user location

--- pages/map/map.js
+++ pages/map/map.js
@@ -9,8 +9,37 @@ Page({
       width: 50,
       height: 50,
       title: "Le Wagon Shanghai",
-      callout: { content: "We're here!", color: "#000000", display:"BYCLICK", padding: 10 }
-    }]
+      callout: { content: "We're here!", fontSize: 15, color: "#000000", display:"BYCLICK", padding: 10 }
+    }],
+    location: {}
+  },
+  findLocation: function (e) {
+    console.log(e)
+    var that = this
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        console.log(latitude)
+        console.log(longitude)
+        that.setData({
+          location: { latitude: latitude, longitude: longitude },
+          markers: [{
+            iconPath: "/image/marker.png",
+            id: 0,
+            latitude: latitude,
+            longitude: longitude,
+            width: 50,
+            height: 50,
+            title: "You buddy",
+            callout: { content: "You are here!", fontSize: 15, color: "#000000", display: "BYCLICK", padding: 10 }
+          }]
+
+        })
+        console.log(that.data.location)
+      }
+    })
   },
   regionchange(e) {
     console.log(e.type)
@@ -19,7 +48,9 @@ Page({
     console.log(e.markerId)
   },
   onLoad: function (options) {
-  
+    this.setData({
+      location: { latitude: "31.219438", longitude:"121.443622"},
+    })
   },
   onReady: function () {
   
