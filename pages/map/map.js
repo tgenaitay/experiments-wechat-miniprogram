fd5eadad977a6b1e3c6a9251c6a1568f7df9ff75
thibault genaitay <tgenaitay@gmail.com>
2017-06-06 22:47:16 +0800

    added buttons for interaction

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,28 +1,21 @@
 // pages/map/map.js
 Page({
   data: {
-    markers: [{
-      iconPath: "/image/marker.png",
-      id: 0,
-      latitude: 31.219438,
-      longitude: 121.443622,
-      width: 50,
-      height: 50,
-      title: "Le Wagon Shanghai",
-      callout: { content: "We're here!", fontSize: 15, color: "#000000", display:"BYCLICK", padding: 10 }
-    }],
-    location: {}
+    location: {} // used to manipulate the map in the view
   },
-  findLocation: function (e) {
-    console.log(e)
+  findUserLocation: function (e) {
     var that = this
     wx.getLocation({
+      // API to locate user
       type: 'wgs84',
       success: function (res) {
+        // storing the user coordinates
         var latitude = res.latitude
         var longitude = res.longitude
         console.log(latitude)
         console.log(longitude)
+        
+        // updating the map location and the marker
         that.setData({
           location: { latitude: latitude, longitude: longitude },
           markers: [{
@@ -33,14 +26,29 @@ Page({
             width: 50,
             height: 50,
             title: "You buddy",
-            callout: { content: "You are here!", fontSize: 15, color: "#000000", display: "BYCLICK", padding: 10 }
+            callout: { content: "You are here!", fontSize: 15, color: "#000000", display: "ALWAYS", padding: 10 }
           }]
-
         })
-        console.log(that.data.location)
       }
     })
   },
+  showWagonLocation: function (e) {
+    var LWlatitude = "31.219438";
+    var LWlongitude = "121.443622";
+    this.setData({
+      location: { latitude: LWlatitude, longitude: LWlongitude },
+      markers: [{
+        iconPath: "/image/marker.png",
+        id: 0,
+        latitude: LWlatitude,
+        longitude: LWlongitude,
+        width: 50,
+        height: 50,
+        title: "Le Wagon",
+        callout: { content: "We are here!", fontSize: 15, color: "#000000", display: "ALWAYS", padding: 10 }
+      }]
+        })
+  },  
   regionchange(e) {
     console.log(e.type)
   },
@@ -48,8 +56,9 @@ Page({
     console.log(e.markerId)
   },
   onLoad: function (options) {
+    // setting the map location on People Square on load of the page
     this.setData({
-      location: { latitude: "31.219438", longitude:"121.443622"},
+      location: { latitude: "31.232711", longitude:"121.47575499999994"},
     })
   },
   onReady: function () {
