fed3428e0b0b9fb78eb569e41388f25735878a41
thibault genaitay <tgenaitay@gmail.com>
2017-06-07 16:31:40 +0800

    wx.request on giphy api

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-// app.js
-// import Leancloud.cn library to store objects on the cloud
-const AV = require('./utils/av-weapp-min.js');
-
-// initialize the Leancloud app https://leancloud.cn/docs/weapp.html
-AV.init({ 
- appId: 'HjPUb8O5jCzs8OPIwWraChtq-gzGzoHsz', 
- appKey: 'sH7ADhsS3e2Wo3N2yCv47Rnd', 
-});
-
 // set an app-level function to grab WeChat user info 
 App({
   onLaunch: function() {
-    console.log("app is launching")
+    console.log("app is launching");
+    var that = this
+
+    var key = "dc6zaTOxFJmzC"
+    var endpoint = "https://api.giphy.com/v1/gifs/"
+    var search = "search?q=funny+cat"
+
+    wx.request({
+      url: endpoint + search +"&api_key="+key,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log("app is loaded with cats");
+        that.globalData.catData = res.data
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    })
+    
+  },
+  getCatData: function (cb) {
+    var that = this
+    typeof cb == "function"
+    cb(this.globalData.catData)
   },
   onShow: function() {
     // Do something when show.
@@ -23,25 +38,7 @@ App({
   onError: function(msg) {
     console.log(msg)
   },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
-    }
-  },
   globalData:{
-    userInfo:null
+    catData:null
   }
 });
---
+++ pages/cats/cats.js
@@ -0,0 +1,43 @@
+// pages/cats/cats.js
+var app = getApp()
+Page({
+  data: {
+    catData: {}
+  },
+  onLoad: function (options) {
+    var that = this
+    // Call the application instance to get data 
+    app.getCatData(function (catData) {
+      // Update data
+      that.setData({
+        catData: catData
+      })
+    })
+    console.log("the page is loaded with cats:");
+    console.log(this.data.catData)
+  },
+  onReady: function () {
+  
+  },
+  onShow: function () {
+  
+  },
+  imgToPhone: function (e) {
+    var gifId = e.target.id
+    console.log(gifId)
+    console.log(this.data.catData.data[gifId].images.fixed_height.url)
+    var filepath = this.data.catData.data[gifId].images.fixed_height.url
+    wx.saveImageToPhotosAlbum({
+      filepath: '',
+      success(res) {
+        console.log(res)
+      }
+    })
+  },
+  onHide: function () {
+  
+  },
+  onShareAppMessage: function () {
+  
+  }
+})
